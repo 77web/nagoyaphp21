c1fd015d4674987aa84da2610db6845af6015131
77web <hishida77web@gmail.com>
2023-10-25 19:18:51 +0900

    落ちるテストを書く

--- src/App.php
+++ src/App.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace Nagoyaphp21\App;
 
-final class App
+class App
 {
+    /**
+     * @param array<int> $inputs
+     * @param int $expected
+     * @return string|null
+     */
+    public function run(array $inputs, int $expected): string|null
+    {
+        return null;
+    }
 }
