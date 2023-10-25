826aafb9ee0d9aab18f9f215d28252bdc93764b1
77web <hishida77web@gmail.com>
2023-10-25 20:34:42 +0900

    人間可読な数式表現についてわる・かけるの前をかっこで囲う

--- src/ExpressionBuilder.php
+++ src/ExpressionBuilder.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Nagoyaphp21\App;
 
+use Nagoyaphp21\App\Operator\Kakeru;
+use Nagoyaphp21\App\Operator\Waru;
+
 class ExpressionBuilder
 {
     public function build(array $inputs, array $operatorCombination): string
@@ -12,7 +15,12 @@ class ExpressionBuilder
         foreach ($inputs as $input) {
             $elements[] = $input;
             if (count($operatorCombination) > 0) {
-                $elements[] = array_shift($operatorCombination);
+                $op = array_shift($operatorCombination);
+                if (($op instanceof Waru || $op instanceof Kakeru) && count($elements) > 1) {
+                    array_unshift($elements, '(');
+                    $elements[] = ')';
+                }
+                $elements[] = $op;
             }
         }
 
